e8c09a17d1cdd95b23aa12b2986285aecd0e48e3
khinthetnwe5 <khinthetnwe24@gmail.com>
2024-07-12 17:31:24 +0630

    create

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinic.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult LogOT()
+        {
+            return View("logO");
+        }
+    }
+}
