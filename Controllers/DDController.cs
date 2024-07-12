f94ed8ac31047f6f88d55f75ad22c7977b869061
Nwezinlinn3 <nwezinlinn65@gmail.com>
2024-07-12 03:54:22 -0700

    implement controller and view

---
+++ Controllers/DDController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinic.Controllers
+{
+    public class DDController : Controller
+    {
+        public IActionResult Clinic()
+        {
+            return View("nwe");
+        }
+    }
+}
---
+++ Controllers/Listcontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinic.Controllers
+{
+    public class Listcontroller : Controller
+    {
+        public IActionResult List()
+        {
+            return View("list");
+        }
+    }
+}
