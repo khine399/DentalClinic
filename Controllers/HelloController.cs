72ce4403c75830bb4b20069626a4416767d2ed4b
Khine399 <bluewynne39@gmail.com>
2024-07-12 16:23:29 +0630

    modifiedlayout and link two menu

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinicController.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+    }
+}
---
+++ Controllers/PendingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinic.Controllers
+{
+    public class PendingController : Controller
+    {
+        public IActionResult PendingList()
+        {
+            return View("Pending");
+        }
+    }
+}
