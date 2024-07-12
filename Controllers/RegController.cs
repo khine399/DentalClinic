413cf3a5a6fcd54749be631c4f9a0c4595cdf108
Khine399 <bluewynne39@gmail.com>
2024-07-12 17:19:13 +0630

    create controller and view

---
+++ Controllers/RegController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinic.Controllers
+{
+    public class RegController : Controller
+    {
+        public IActionResult Reg()
+        {
+            return View("Register");
+        }
+    }
+}
